9cbbc2d4d6b92201234b386be11abb38173f2df9
Carljhn <zoletacarljohn1@gmail.com>
2023-06-04 22:27:30 +0800

    Second Commit: Added the for loop that interprets and finds the largest number in the array.

--- SD_BSCOE_2-6_ZOLETA_CarlJohn_JavaLang_LogicForm/max_value_array.java
+++ SD_BSCOE_2-6_ZOLETA_CarlJohn_JavaLang_LogicForm/max_value_array.java
@@ -2,8 +2,15 @@
 
 public class max_value_array {
     public static void main(String[] args) {
-        int[] numbers = {1, 3, 5, 7, 9, 2, 4, 6, 8};
+        int[] numbers = {1, 3, 5, 7, 9, 2, 4, 6, 81};
         int maxValue = numbers[0];
+
+        // Implementing for loop to find the largest number in the given array
+        for(int i = 0; i < numbers.length; i++){
+            if(numbers[i] > maxValue){
+                maxValue = numbers[i];
+            }
+        }
 
         System.out.println("=============================================");
         System.out.println("The maximum value in the array is: " + maxValue);
