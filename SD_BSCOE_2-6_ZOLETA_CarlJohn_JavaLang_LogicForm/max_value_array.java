e2a8a0c1e640e30db5744d51158ff95b939dca4f
Carljhn <zoletacarljohn1@gmail.com>
2023-06-04 22:17:13 +0800

    Initial Commit: I have added the basic requirements for this program.

---
+++ SD_BSCOE_2-6_ZOLETA_CarlJohn_JavaLang_LogicForm/max_value_array.java
@@ -0,0 +1,12 @@
+// Finding the maximum value in an array
+
+public class max_value_array {
+    public static void main(String[] args) {
+        int[] numbers = {1, 3, 5, 7, 9, 2, 4, 6, 8};
+        int maxValue = numbers[0];
+
+        System.out.println("=============================================");
+        System.out.println("The maximum value in the array is: " + maxValue);
+        System.out.println("=============================================");
+    }
+}
