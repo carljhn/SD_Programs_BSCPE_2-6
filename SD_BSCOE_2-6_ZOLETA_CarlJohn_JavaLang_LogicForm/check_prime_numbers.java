e1a204173918b735b5dd279e0a748a76fdf03bd1
Carljhn <zoletacarljohn1@gmail.com>
2023-06-04 22:40:55 +0800

    Initial Commit: Added the class and required output for this program.

---
+++ SD_BSCOE_2-6_ZOLETA_CarlJohn_JavaLang_LogicForm/check_prime_numbers.java
@@ -0,0 +1,15 @@
+// Checking for a Prime Number
+
+public class check_prime_numbers {
+    public static void main(String[] args) {
+        int num = 29; // This is the number to be tested
+        boolean isPrime = true;
+
+        if(isPrime) {
+            System.out.println(num + " is a prime number.");
+        }
+        else{
+            System.out.println(num + " is not a prime number.");
+        }
+    }
+}
