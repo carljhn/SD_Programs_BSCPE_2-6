ccadae0025dc72c14e22f660341a11868e77ec3b
Carljhn <zoletacarljohn1@gmail.com>
2023-06-04 22:53:01 +0800

    Second Commit: Added the process of checking if the number is prime or not by implementing for loop.

--- SD_BSCOE_2-6_ZOLETA_CarlJohn_JavaLang_LogicForm/check_prime_numbers.java
+++ SD_BSCOE_2-6_ZOLETA_CarlJohn_JavaLang_LogicForm/check_prime_numbers.java
@@ -5,11 +5,23 @@ public class check_prime_numbers {
         int num = 29; // This is the number to be tested
         boolean isPrime = true;
 
+        // Implementing for loop and some conditional statements to test is the number is a prime or not.
+        for(int i = 2; i < num; i++) {
+            if(num%i==0){
+                isPrime = false;
+                break;
+            }
+        }
+
         if(isPrime) {
+            System.out.println("=============================================");
             System.out.println(num + " is a prime number.");
+            System.out.println("=============================================");
         }
         else{
+            System.out.println("=============================================");
             System.out.println(num + " is not a prime number.");
+            System.out.println("=============================================");
         }
     }
 }
