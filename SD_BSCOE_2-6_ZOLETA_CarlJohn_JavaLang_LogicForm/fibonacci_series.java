368ccc6771c42d6db2f35163ba25572abcd46ff4
Carljhn <zoletacarljohn1@gmail.com>
2023-06-05 19:44:07 +0800

    Second Commit: Finished the program by adding the code block that interprets and processes the required output.

--- SD_BSCOE_2-6_ZOLETA_CarlJohn_JavaLang_LogicForm/fibonacci_series.java
+++ SD_BSCOE_2-6_ZOLETA_CarlJohn_JavaLang_LogicForm/fibonacci_series.java
@@ -1,8 +1,20 @@
 public class fibonacci_series {
     public static void main(String[] args) {
+        int t1 = 0;
+        int t2 = 1;
         int num = 10;
-        int t1 = 0, t2 = 1;
+        int t3;
 
         System.out.println("First " + num + " terms: ");
+        System.out.println(t1);
+        System.out.println(t2);
+
+        // Implementing for loop to obtain the fibonacci sequence 
+        for(int i = 1; i < num; i++){
+            t3 = t1 + t2;
+            System.out.println(t3);
+            t1 = t2;
+            t2 = t3;
+        }
     }
 }
