d0b5cee96ccd2b94c0953f3150232e075d26d112
Carljhn <zoletacarljohn1@gmail.com>
2023-06-05 18:28:55 +0800

    Initial Commit: Added the class for this program.

---
+++ SD_BSCOE_2-6_ZOLETA_CarlJohn_JavaLang_LogicForm/fibonacci_series.java
@@ -0,0 +1,8 @@
+public class fibonacci_series {
+    public static void main(String[] args) {
+        int num = 10;
+        int t1 = 0, t2 = 1;
+
+        System.out.println("First " + num + " terms: ");
+    }
+}
